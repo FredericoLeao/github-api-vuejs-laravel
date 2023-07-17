d73aeadf96af520b636408ce20159640cb6119ff
Mario F. Leão <fredericoleaob@gmail.com>
2023-07-17 14:40:54 -0300

    Create User List Row as a component

--- appjs/src/views/User/List/Index.vue
+++ appjs/src/views/User/List/Index.vue
@@ -1,36 +1,17 @@
 <template>
   <div class="row mb-2" v-for="(user,id) in storeUser.getUsers">
-    <div class="col-12">
-      <div class="card">
-        <router-link :to="{ name: 'UserDetails', params: { login: user.login } }">
-          <div class="row">
-            <div class="col-sm-3">
-              <img class="img-fluid" :src="user.avatar_url" />
-            </div>
-            <div class="col-sm-9">
-              <div class="card-body">
-                <h5 class="card-title">{{ user.name }}</h5>
-                <p class="card-text">
-                  <small class="text-muted">Usuário desde {{ formatDate(user.created_at) }}</small>
-                  <span class="float-end">{{ user.public_repos }}</span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </router-link>
-      </div>
-    </div>
+    <ListRow :user="user" />
   </div>
 </template>
 
 <script>
 import { onMounted, defineComponent } from 'vue';
 import { useUserStore } from '@/store/User'
-import CommonMixin from '@/mixins/Common'
+import ListRow from '@/views/User/List/Row.vue'
 
 export default defineComponent({
   name: "UserList",
-  mixins: [CommonMixin],
+  components: { ListRow },
   setup () {
     const storeUser = useUserStore()
     onMounted(() => {
---
+++ appjs/src/views/User/List/Row.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="col-12">
+    <div class="card">
+      <router-link :to="{ name: 'UserDetails', params: { login: user.login } }">
+        <div class="row">
+          <div class="col-sm-3">
+            <img class="img-fluid" :src="user.avatar_url" />
+          </div>
+          <div class="col-sm-9">
+            <div class="card-body">
+              <h5 class="card-title">{{ user.name }}</h5>
+              <p class="card-text">
+                <small class="text-muted">Usuário desde {{ formatDate(user.created_at) }}</small>
+                <span class="float-end">{{ user.public_repos }}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import CommonMixin from '@/mixins/Common'
+
+export default defineComponent({
+  name: "ListRow",
+  props: ["user"],
+  mixins: [CommonMixin],
+  setup (props) {
+    const user = props.user
+    return { user }
+  }
+})
+</script>
